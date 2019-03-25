c742c751c90ecbf02b70ab5f4a6898d811d3991a
Andrei <andreiazotov@gmail.com>
2019-03-25 09:05:33 +0300

    go parsing json

---
+++ GoParsingJSON/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+)
+
+// Users struct
+type Users struct {
+	Users []User `json:"users"`
+}
+
+// User struct
+type User struct {
+	Name   string `json:"name"`
+	Type   string `json:"type"`
+	Age    int    `json:"int"`
+	Social Social `json:"social"`
+}
+
+// Social struct
+type Social struct {
+	Facebook string `json:"facebook"`
+	Twitter  string `json:"twitter"`
+}
+
+func main() {
+	jsonFile, err := os.Open("data.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Open file successfully")
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	var users Users
+	json.Unmarshal(byteValue, &users)
+
+	for i := 0; i < len(users.Users); i++ {
+		fmt.Println("User Type: " + users.Users[i].Type)
+		fmt.Println("User Age: " + strconv.Itoa(users.Users[i].Age))
+		fmt.Println("User Name: " + users.Users[i].Name)
+		fmt.Println("Facebook Url: " + users.Users[i].Social.Facebook)
+	}
+
+	var usersUnstructured map[string]interface{}
+	json.Unmarshal(byteValue, &usersUnstructured)
+	fmt.Println(usersUnstructured["users"])
+}
